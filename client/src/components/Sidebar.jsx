1de550f332f3f7e8383b39e0f618115102b8d878
Ramani <rushikramani@csu.fullerton.edu>
2023-03-01 14:22:35 -0800

    final frontend changes

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -34,10 +34,10 @@ import FlexBetween from "./FlexBetween";
 import profileImage from "assets/profile.jpeg";
 
 const navItems = [
-  // {
-  //   text: 'Dashboard',
-  //   icon: <HomeOutlined />
-  // },
+  {
+    text: 'Dashboard',
+    icon: <HomeOutlined />
+  },
  
   {
     text: "Client Facing",
